7a3eb5016fdb37d4349767dd4867e1409b10d48a
unknown <info@chenwebsites.com>
2023-03-07 23:55:34 -0500

    Building Process

--- src/main/java/com/spring/products/controller/ProductsController.java
+++ src/main/java/com/spring/products/controller/ProductsController.java
@@ -1,8 +1,17 @@
 package com.spring.products.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.products.model.ProductItem;
+import com.spring.products.repository.ProductsRepository;
 
 @RestController
 @RequestMapping("/products")
@@ -10,5 +19,20 @@ public class ProductsController {
 
 	@Autowired
 	ProductsRepository repository;
+
+	public ProductsController(ProductsRepository repository) {
+		this.repository = repository;
+	}
+	
+	@PostMapping
+	public ProductItem createProduct(@RequestBody ProductItem product) {
+		return repository.save(product);
+	}
+	
+	@GetMapping("/get-{brand}")
+	public List<ProductItem> getBrandList(@PathVariable String brand){
+		
+		return repository.getProductByBrand(brand);
+	}
 	
 }
--- src/main/java/com/spring/products/repository/ProductsRepository.java
+++ src/main/java/com/spring/products/repository/ProductsRepository.java
@@ -1,4 +1,7 @@
-package com.spring.products.controller;
+package com.spring.products.repository;
+
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.repository.CrudRepository;
 
@@ -6,4 +9,5 @@ import com.spring.products.model.ProductItem;
 
 public interface ProductsRepository extends CrudRepository<ProductItem, Long> {
 
+	List<ProductItem> getProductByBrand(String brand);
 }
